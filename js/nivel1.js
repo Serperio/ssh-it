2c4760b58f4c516fc7f3dd3b7cc49aa08d8edaff
Koba364 <61332602+Koba364@users.noreply.github.com>
2020-03-24 01:22:00 -0300

    Add files via upload

---
+++ js/nivel1.js
@@ -0,0 +1,19 @@
+// JavaScript source code
+function level1() {
+        var level_1 = document.getElementById("contra").value;
+        var contra = document.getElementById("lvl1");
+        contra.classList.remove('alert-success','alert-secondary','alert-danger');
+        contra.classList.add('alert');
+        if (level_1 == "v5x67rmnNzUhS0qfxfCEzi2jup22n5IjJTacz8ID4") {
+            contra.classList.add('alert-success');
+            contra.innerHTML='<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+        }
+        else if (level_1 == "") {
+            contra.classList.add('alert-secondary');
+            contra.innerHTML='<strong>Vacío!</strong> Ingrese una contraseña.';
+        }
+        else  {
+            contra.classList.add('alert-danger');
+            contra.innerHTML='<strong>Incorrecto!</strong> Intentelo de nuevo :C';}
+            
+    }
