3731f56bbf32a784948766fe00bb7ee7f5a5070d
Serperio <61319299+Serperio@users.noreply.github.com>
2020-03-24 21:13:50 -0300

    Add files via upload

--- js/nivel1.js
+++ js/nivel1.js
@@ -1,10 +1,29 @@
 // JavaScript source code
-function level1() {
+function level0a() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "v5x67rmnNzUhS0qfxfCEzi2jup22n5IjJTacz8ID4") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level1a() {
         var level_1 = document.getElementById("contra").value;
         var contra = document.getElementById("lvl1");
         contra.classList.remove('alert-success','alert-secondary','alert-danger');
         contra.classList.add('alert');
-        if (level_1 == "v5x67rmnNzUhS0qfxfCEzi2jup22n5IjJTacz8ID4") {
+    if (level_1 == "nLNhMzs0WGl3PQO5Z8A3hFIdKMadRnuhOuNn66g17bM") {
             contra.classList.add('alert-success');
             contra.innerHTML='<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
         }
@@ -16,4 +35,365 @@ function level1() {
             contra.classList.add('alert-danger');
             contra.innerHTML='<strong>Incorrecto!</strong> Intentelo de nuevo :C';}
             
+}
+function level2a() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "oKHWtK4xfnHt8lcjryU+rZ9zKsaCsVMruM8x6TTntxI") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
     }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level3a() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "mxu80dVSdnIRiH8KAsPewxUv3d4bw5gwPvQq7WIXKL8") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level4a() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "kreTJCAkVKdKgvjyT3gBP5nyLYTdv7LbYUhRRFCB9Bo") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level5a() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "1933592") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level6a() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "yKO6nlTN+QBDTw27qxzvHiNEAiWEgkXFl0QVNopudv0") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level7a() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "FtXUzSB5k1+AW8vvs9cH3sjmlKVv0AJHd5PKRkhG8EI") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level8a() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "QfN3GZjuq5S5tMVKFHleHIdXfRqKhte2nmPTjd6wA") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level1b() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "00LGWMhmUVvGgvBIWfqipNcX9RSPjq3FNdcKIEM28") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level2b() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "JKGZ42Qy1d22TBDQirLfX8ucC3bsVQA1QmheB61eU0") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level3b() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "Hw8d8PRneO63ul3jUclryONuydqx8+vaoHi73t3uY") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level4b() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "d8QKBYl068Q4oQaR6ASF9RCUdQ7RXEa9QxBfYdJQ") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level5b() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "Oe9XZVPQdgsLWQRun29VXkvOMwu3YMwg9qZFlbyKyc") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+
+function level6b() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "DpwdlgYyKmVJf2WeCOiwV1mtM92z4KCwVFPc16hZPI") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level7b() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "lNJDjpxFKHCTqw207IQey2+gK3M6UpCM0cNI64fE4M") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level8b() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "ZOGKSjKiStFG3cE47tpDkU6eQv4dAjhfWS67CwhKU") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+}
+function level1c() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "RMy2OaZ8HoepvUwKtJaA5irhqTOcabPOtIXS3U2xs") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level2c() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "1b6BdUvVUFB1KApPJiGoqt6gKt980fugbhd6l7SBRjA") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level3c() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "5sOTdhOYFwtengI7i0iigP5BmNS2LCO0pzbhyp27Zc") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
+function level4c() {
+    var level_1 = document.getElementById("contra").value;
+    var contra = document.getElementById("lvl1");
+    contra.classList.remove('alert-success', 'alert-secondary', 'alert-danger');
+    contra.classList.add('alert');
+    if (level_1 == "bZMyYysodZySymjpJcaeMtZgUUb2AUjGFb6P4NFCF0") {
+        contra.classList.add('alert-success');
+        contra.innerHTML = '<strong>Correcto!</strong> Puedes acceder al siguiente nivel! :D';
+    }
+    else if (level_1 == "") {
+        contra.classList.add('alert-secondary');
+        contra.innerHTML = '<strong>Vacío!</strong> Ingrese una contraseña.';
+    }
+    else {
+        contra.classList.add('alert-danger');
+        contra.innerHTML = '<strong>Incorrecto!</strong> Intentelo de nuevo :C';
+    }
+
+}
